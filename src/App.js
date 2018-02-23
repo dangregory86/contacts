3d78133dd8a846d75b0ba618bbce40ef3e483035
Dan Gregory <danielgregory1986@googlemail.com>
2018-02-23 17:09:14 +0000

    Continuing with the code along
    Added a router.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom'
+import ListContacts from './listcontacts';
+import CreateContact from './CreateContact'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
+  state = {
+    contacts: []
+  }
+  componentDidMount(){
+    ContactsAPI.getAll().then((contacts) =>{
+      this.setState({contacts});
+    });
+  }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+
+    ContactsAPI.remove(contact);
+  }
+
   render() {
-    return <div>Hello world</div>
+    return (
+      <div className='app'>
+        <Route exact path='/' render={() => (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+          />
+      )}/>
+    <Route path='/create' component={CreateContact}/>
+      </div>
+    )
   }
 }
 
